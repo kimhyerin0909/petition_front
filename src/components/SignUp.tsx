564a131815f9e2f84729441f6a965ea4aba5ad1b
kimhyerin0909 <khyerin0909@gmail.com>
2022-05-26 22:19:55 +0900

    NEW

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SignUp() {
+
+    return (
+        <div className='signup-box'>
+            <section>
+                <img src='./main-logo.png' alt='logo' />
+            </section>
+        </div>
+    )
+}
