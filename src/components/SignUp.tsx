b7825c698c805dd7e55b76ed7d854e30ead0fdca
kimhyerin0909 <khyerin0909@gmail.com>
2022-05-28 22:19:33 +0900

    FEAT :: Signup 틀

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -4,9 +4,16 @@ export default function SignUp() {
 
     return (
         <div className='signup-box'>
-            <section>
+            <section className='signup-leftside'>
                 <img src='./main-logo.png' alt='logo' />
             </section>
+            <form>
+                <input type="email" placeholder='이메일을 입력해 주세요.' />
+                <input type="password" placeholder='비밀번호를 입력해 주세요.' />
+                <input type="password" placeholder='비밀번호를 다시 한 번 입력해주세요.' />
+                <input type="search" placeholder='학교를 선택해주세요.'/>
+                <button>회원가입</button>
+            </form>
         </div>
     )
 }
