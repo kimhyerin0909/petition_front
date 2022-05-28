3639767c2710e848215827c35aaef6d04c588332
kimhyerin0909 <khyerin0909@gmail.com>
2022-05-28 23:14:50 +0900

    DESIGN :: 회원가입 퍼블리싱 끗.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './style.css';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function SignUp() {
+    const [color, setColor] = useState("orange");
+
+    setInterval(() => {
+        if(color === "orange") setColor("pink")
+        else if(color === "pink") setColor("purple")
+        else if(color === "purple") setColor("orange");
+
+    }, 3000);
 
     return (
         <div className='signup-box'>
@@ -8,6 +16,7 @@ export default function SignUp() {
                 <img src='./main-logo.png' alt='logo' />
             </section>
             <form>
+                <h1>회원가입</h1>
                 <input type="email" placeholder='이메일을 입력해 주세요.' />
                 <input type="password" placeholder='비밀번호를 입력해 주세요.' />
                 <input type="password" placeholder='비밀번호를 다시 한 번 입력해주세요.' />
