1ae0dfdbc085144cb62db56de604553dab27af3e
kimhyerin0909 <khyerin0909@gmail.com>
2022-05-29 22:37:29 +0900

    FEAT :: 배경 색 자동으로 바꾸기 기능,,, but 렌더링이 점점 늘어남

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 export default function SignUp() {
-    const [color, setColor] = useState("orange");
+    const [color, setColor ]= useState("#F9CC82");
+    const colorChange = useRef<any>(null);
 
     setInterval(() => {
-        if(color === "orange") setColor("pink")
-        else if(color === "pink") setColor("purple")
-        else if(color === "purple") setColor("orange");
-
-    }, 3000);
+        console.log(1)
+        color === "#F9CC82" ? setColor("#E2B4BA") : setColor("#F9CC82");
+        
+        // console.log(color)
+        // colorChange.current.style = `background-color: ${color};`
+        // border-bottom: 2px solid ${color};
+    }, 3000)
 
     return (
         <div className='signup-box'>
-            <section className='signup-leftside'>
+            <div ref={colorChange} id='leftside' className='signup-leftside'>
                 <img src='./main-logo.png' alt='logo' />
-            </section>
+            </div>
             <form>
                 <h1>회원가입</h1>
-                <input type="email" placeholder='이메일을 입력해 주세요.' />
-                <input type="password" placeholder='비밀번호를 입력해 주세요.' />
-                <input type="password" placeholder='비밀번호를 다시 한 번 입력해주세요.' />
-                <input type="search" placeholder='학교를 선택해주세요.'/>
+                <input className='input' type="email" placeholder='이메일을 입력해 주세요.' />
+                <input className='input' type="password" placeholder='비밀번호를 입력해 주세요.' />
+                <input className='input' type="password" placeholder='비밀번호를 다시 한 번 입력해주세요.' />
+                <input className='input' type="search" placeholder='학교를 선택해주세요.'/>
                 <button>회원가입</button>
             </form>
         </div>
